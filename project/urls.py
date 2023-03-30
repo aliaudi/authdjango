76221be32e561923f70506ceaef3dd57b2f2d18c
ali <aaudi2974@gmail.com>
2023-03-30 11:32:27 -0500

    add signup,profile,profileedit

--- project/urls.py
+++ project/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
 
     path('admin/', admin.site.urls),
+    path('accounts/', include('accounts.urls',namespace='accounts')),
 ]
